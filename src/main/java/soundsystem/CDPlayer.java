e48ab506846319706301add0f1955a93a87a5e16
zolinz <zoltankovacs_nz@yahoo.co.nz>
2017-07-26 12:07:53 +1000

    added more comments and autowiring

--- src/main/java/soundsystem/CDPlayer.java
+++ src/main/java/soundsystem/CDPlayer.java
@@ -1,5 +1,6 @@
 package soundsystem;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,12 @@ public class CDPlayer {
 
     private CD cd;
 
+  //  Put succinctly, autowiring is a means of letting Spring automatically satisfy a bean’s
+  //  dependencies by finding other beans in the application context that are a match to
+  //  the bean’s needs. To indicate that autowiring should be performed, you can use
+  //  Spring’s @Autowired annotation.
+
+    @Autowired
     public CDPlayer(CD cd){
         this.cd = cd;
     }
