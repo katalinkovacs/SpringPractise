c7d144bd1218dbf7e3dd62478a43502021b1dc4e
DESKTOP-6SPUU5F\kati <katalinkovacs_nz@yahoo.com>
2017-07-27 14:42:30 +1000

    added some notes

--- src/main/java/soundsystem/SebastianBach.java
+++ src/main/java/soundsystem/SebastianBach.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Primary
 @Component
+// @Component  -->  <bean id = "SebastianBach" class = "soundsystem.SebastianBach"></bean>
 public class SebastianBach implements CD{
 
     public String play(){
